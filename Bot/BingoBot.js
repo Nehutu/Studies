fc1022dd8be2ba41677310342801c962fe2897a7
Renat Valitov <140712221+Nehutu@users.noreply.github.com>
2023-08-30 15:08:16 +0300

    Update BingoBot.js

--- Bot/BingoBot.js
+++ Bot/BingoBot.js
@@ -1,1 +1,35 @@
-https://github.com/Nehutu/Study/blob/a84c5d0d9dcfbe5ac560529acac80198de528026/Bot/Bingo.js
+// ==UserScript==
+// @name         Bot for Bing
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Learning to Bot.
+// @author       You
+// @match        https://www.bing.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+let searchInput = document.getElementById("sb_form_q");
+let button = document.getElementById("search_icon");
+let links = document.links
+let keywords = ["10 самых популярных шрифтов от Google",
+                "Отключение редакций и ревизий в WordPress",
+                "Вывод произвольных типов записей и полей в WordPress"];
+let keyword = keywords[getRandom(0, keywords.length)];
+
+if(button != undefined) {
+  searchInput.value = keyword;
+  button.click();
+  } else {
+  for (let i = 0; i < links.length; i++) {
+    if(links[i].href.indexOf("napli.ru") != -1) {
+      console.log("Got it " + links[i]);
+      links[i].click();
+      break;
+    }
+  }
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
